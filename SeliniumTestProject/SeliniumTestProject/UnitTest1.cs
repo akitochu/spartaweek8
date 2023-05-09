8f4442b755cbfe73ba1d3f4ff0038e2843e4497d
DESKTOP-DJK1QF7\akito <akitochu@gmail.com>
2023-05-09 13:30:40 +0100

    initial commit week 8

---
+++ SeliniumTestProject/SeliniumTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeliniumTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
